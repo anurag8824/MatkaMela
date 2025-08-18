9892ba11ad7c7d898bab6a49f87c6598ef251770
rahulk67 <rahulkumawat6767@gmail.com>
2025-08-18 12:13:34 +0530

    update sms

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,21 +12,21 @@ dotenv.config();
 //     port: 3306
 // });
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'matka',
-    port: process.env.DB_PORT || 3306,
-    // timezone: '+05:30'
-});
 // const pool = mysql.createPool({
 //     host: process.env.DB_HOST || 'localhost',
-//     user: process.env.DB_USER || 'myuser',
-//     password: process.env.DB_PASSWORD || 'mypassword',
+//     user: process.env.DB_USER || 'root',
+//     password: process.env.DB_PASSWORD || '',
 //     database: process.env.DB_NAME || 'matka',
 //     port: process.env.DB_PORT || 3306,
+//     // timezone: '+05:30'
 // });
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'myuser',
+    password: process.env.DB_PASSWORD || 'mypassword',
+    database: process.env.DB_NAME || 'matka',
+    port: process.env.DB_PORT || 3306,
+});
 
 
 export default pool;
