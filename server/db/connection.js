0ec7903c913df4557df0d74faf457eb2c2c3c4b2
rahulk67 <rahulkumawat6767@gmail.com>
2025-09-08 12:31:11 +0530

    fix bet time

--- server/db/connection.js
+++ server/db/connection.js
@@ -27,6 +27,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || 'mypassword',
     database: process.env.DB_NAME || 'matka',
     port: process.env.DB_PORT || 3306,
+    timezone: '+05:30' ,
 });
 
 export default pool;
