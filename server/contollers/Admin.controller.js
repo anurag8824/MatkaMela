cd9cd627b870e13a0c4b8e32d2c8e4b604efd3d6
rahulk67 <rahulkumawat6767@gmail.com>
2025-09-09 15:16:27 +0530

    fix times of w

--- server/contollers/Admin.controller.js
+++ server/contollers/Admin.controller.js
@@ -1103,8 +1103,8 @@ export const getWinningNumberold = async (req, res) => {
     try {
       // Step 1: Check if any bets exist with given RESULT
       const resultQuery = `
-        SELECT 
-          ID, DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
+        SELECT  
+          ID, DATE_FORMAT(CONVERT_TZ(DATE_TIME,'+00:00','+05:30'), '%Y-%m-%d %H:%i:%s') AS DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
         FROM bets
         WHERE DATE(DATE_TIME) = ?
           AND GAME_ID = ?
@@ -1123,7 +1123,7 @@ export const getWinningNumberold = async (req, res) => {
         const placeholders = digits.map(() => "?").join(",");
         const digitQuery = `
           SELECT 
-            ID, DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
+            ID, DATE_FORMAT(CONVERT_TZ(DATE_TIME,'+00:00','+05:30'), '%Y-%m-%d %H:%i:%s') AS DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
           FROM bets
           WHERE DATE(DATE_TIME) = ?
             AND GAME_ID = ?
@@ -1160,7 +1160,7 @@ export const getWinningNumberold = async (req, res) => {
       // Step 2: If no result match, check by NUMBER
       const numberQuery = `
         SELECT 
-          ID, DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
+          ID, DATE_FORMAT(CONVERT_TZ(DATE_TIME,'+00:00','+05:30'), '%Y-%m-%d %H:%i:%s') AS DATE_TIME, PHONE, POINT, NUMBER, GAME_ID, GAME, TYPE, STATUS, RESULT
         FROM bets
         WHERE DATE(DATE_TIME) = ?
           AND GAME_ID = ?
